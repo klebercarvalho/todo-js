fb578f2d094f4a80babf3ee24ceb454577223d39
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2020-04-02 13:35:20 -0300

    create, remove, save todos

--- public/todos.js
+++ public/todos.js
@@ -1,36 +1,50 @@
-const listElement = document.querySelector('#app ul')
-const inputElement = document.querySelector('#app input')
-const buttonElement = document.querySelector('#app button')
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
 
-const todos = [
-    'Fazer Café',
-    'Estudar Javascript',
-    'Trabalhar',
-]
+const todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
-function renderTodos() {
+function renderTodos(){
 
-    listElement.innerHTML = ''
+    listElement.innerHTML = '';
 
-    for (const todo of todos) {
-        let todoElement = document.createElement('li')
-        let todoText = document.createTextNode(todo)
+    for (todo of todos ){
 
-        todoElement.appendChild(todoText)
-        listElement.appendChild(todoElement)
-        
+        const todoElement = document.createElement('li');
+        const todoText = document.createTextNode(todo);
+
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        const pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick','deleteTodo('+ pos +')');
+        const linkText = document.createTextNode(' Excluir');
+
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+
     }
 }
 
-renderTodos()
+renderTodos();
 
-function addTodo() {
-    let todoText = inputElement.value
-
-    todos.push(todoText)
-    inputElement.value = ''
-    renderTodos()
-
+function addTodo(){
+    const todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
 }
 
-buttonElement.onclick = addTodo
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
